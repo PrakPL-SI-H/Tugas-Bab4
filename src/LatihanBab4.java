4c6fc143062329a3f64f7fa5f23964a1dd2fc4e8
Nofry <nofryarman@gmail.com>
2016-03-26 23:27:45 -0700

    pembuatan variabel dan constructor

--- src/LatihanBab4.java
+++ src/LatihanBab4.java
@@ -1,13 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
+class Buku{
+    private String nama;
+    private String alamat;
+    private String nomerTelp;
+    private String email;
+    public Buku(){
+        this.nama = "";
+        this.alamat = "";
+        this.nomerTelp = "";
+        this.email =  "";
+    }
+}
+public class LatihanBab4 {
     public static void main(String[] args) {
         System.out.println("tess");
         
