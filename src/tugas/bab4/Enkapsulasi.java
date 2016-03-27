f6674acf08171f61eff60c29aeccb658ecbae975
arifardhan <ariframadhan902@yahoo.com>
2016-03-27 10:12:09 +0700

    Membuat accessor, muator method dan constructor pada class

--- src/tugas/bab4/Enkapsulasi.java
+++ src/tugas/bab4/Enkapsulasi.java
@@ -3,7 +3,48 @@ package Praktikum4;
 // @author Arif Ramadhan
 public class Enkapsulasi {
 
-    String name, alamat, notelp, email;
+    public String name, address, numtlp, email;
+    private String nama, alamat, notlp, e_mail;
+
+    public Enkapsulasi(String name, String address, String numtlp, String email) {
+        this.name = name;
+        this.address = address;
+        this.numtlp = numtlp;
+        this.email = email;
+    }
+
+    public void setNama(String nam) {
+
+    }
+
+    public void setAlamat(String alama) {
+
+    }
+
+    public void setNotlp(String no) {
+
+    }
+
+    public void setEmail(String mail) {
+
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getNotlp() {
+        return notlp;
+    }
+
+    public String getEmail() {
+        return e_mail;
+    }
+    
     
     
 }
