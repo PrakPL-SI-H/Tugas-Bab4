66e601d3b946d2129ceb16a932c3175918c47617
arifardhan <ariframadhan902@yahoo.com>
2016-03-27 10:47:47 +0700

    Mengisntans objek pada main class

--- src/tugas/bab4/EnkapsulasiMain.java
+++ src/tugas/bab4/EnkapsulasiMain.java
@@ -1,7 +1,16 @@
 package Praktikum4;
 
  // @author Arif Ramadhan
-
 public class EnkapsulasiMain {
-    
+
+    public static void main(String[] args) {
+        String nama,alamat,notlp,email;
+        Enkapsulasi o1=new Enkapsulasi("Arif Ramadhan","Jl.Kertoraharjo no.24 Malang","085817602880","ariframadhan902@yahoo.com");
+        Enkapsulasi o2=new Enkapsulasi("Dikny Asti Khaerunnisa","Jl.Baros no.1 Serang","087718614780","diknyasti@gmail.com");
+        Enkapsulasi o3=new Enkapsulasi("Gizan Dharmawan","Jl.Siliwangi no.4 Rangkasbitung","08134503901","gizandh@gmail.com");
+        Enkapsulasi o4=new Enkapsulasi("Rian Permana","Jl.Raya denpasar no.7 Denpasar","089677078809","riianp@yahoo.com");
+        Enkapsulasi o5=new Enkapsulasi("Putri Anggraeni","Jl.Kelapa dua no.11 Jakarta Utara","089613452567","putrianggraeni@gmail.com");
+        
+        
+    }
 }
