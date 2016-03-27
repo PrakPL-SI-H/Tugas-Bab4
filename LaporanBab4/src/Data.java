8dd48ffedc5badc1a5ae1ea787249a097b3f3a9e
FajarAH <akbaransyah@gmail.com>
2016-03-27 13:32:33 +0700

    membuat class Data
    membuat acessor dan mutator variabel nama

---
+++ LaporanBab4/src/Data.java
@@ -0,0 +1,10 @@
+public class Data {
+    private String nama;
+    
+    public void setNama (String name){
+        this.nama = name;
+    }
+    public String getNama (){
+        return nama;
+    }
+}
