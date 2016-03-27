5763d18af492c602a4818d29b4683cd5763a2698
TAMP31 <mahadikaandre@gmail.com>
2016-03-27 19:39:26 +0700

    membuat masukkan ke 2 dan menyelesaikan class

--- src/tugas/bab4/MainAlamat.java
+++ src/tugas/bab4/MainAlamat.java
@@ -5,14 +5,29 @@ public class MainAlamat {
         Scanner in = new Scanner (System.in);
         BukuAlamat z1 = new BukuAlamat();
         BukuAlamat z2 = new BukuAlamat();
-        System.out.println("Masukkan nama 1 : ");
+        System.out.print("Masukkan nama 1          : ");
         z1.setNama(in.nextLine());
-        System.out.println("Masukkan alamat 1 : ");
+        System.out.print("Masukkan alamat 1        : ");
         z1.setAlamat(in.nextLine());
-        System.out.println("Masukkan nomor telepon 1 : ");
+        System.out.print("Masukkan nomor telepon 1 : ");
         z1.setNotel(in.next());
-        System.out.println("Masukkan alamat e-mail 1 : ");
+        System.out.print("Masukkan alamat e-mail 1 : ");
         z1.setEmail(in.next());
+        
+        System.out.println("-----------------------------");
+        
+        System.out.print("Masukkan nama 2          : ");
+        in.nextLine();
+        z2.setNama(in.nextLine());
+        System.out.print("Masukkan alamat 2        : ");
+        z2.setAlamat(in.nextLine());
+        System.out.print("Masukkan nomor telepon 2 : ");
+        z2.setNotel(in.next());
+        System.out.print("Masukkan alamat e-mail 2 : ");
+        z2.setEmail(in.next());
+        System.out.println("-----------------------------");
+        z1.drome();
+        z2.drome();
     }
     
 }
