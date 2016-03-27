1c4a6eb9752647dd3a0c6f0f39fa95bd9282ee8f
Nofry <nofryarman@gmail.com>
2016-03-26 23:31:56 -0700

    pembuatan method set get atau accescor dan mutator

--- src/LatihanBab4.java
+++ src/LatihanBab4.java
@@ -9,6 +9,30 @@ class Buku{
         this.nomerTelp = "";
         this.email =  "";
     }
+    public void setName(String nama){
+        this.nama = nama;
+    }
+    public void setAddres(String alamat){
+        this.alamat = alamat;
+    }
+    public void setNumber(String nomerTelp){
+        this.nomerTelp = nomerTelp;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getName(){
+        return nama;
+    }
+    public String getAddres(){
+        return alamat;
+    }
+    public String getNumber(){
+        return nomerTelp;
+    }
+    public String getEmail(){
+        return email;
+    }
 }
 public class LatihanBab4 {
     public static void main(String[] args) {
