b5bfb49d29c83267122f7ba6eaf3f11aa42dcc7d
FajarAH <akbaransyah@gmail.com>
2016-03-29 06:31:23 +0700

    Edit source code
    membuat perulangan agar user bisa menginputkan lebih dari 1  data

--- LaporanBab4/src/MainData.java
+++ LaporanBab4/src/MainData.java
@@ -1,19 +1,35 @@
 
 import java.util.Scanner;
+
 public class MainData {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        Data user = new Data();
-        
-        System.out.print("Masukkan Nama : ");
-        user.setNama(in.nextLine());
-        System.out.print("Masukkan Alamat : ");
-        user.setAlamat(in.nextLine());
-        System.out.print("Masukkan Nomor Telepon : ");
-        user.setNotelepon(in.nextLine());
-        System.out.print("Masukkan Alamat E-mail : ");
-        user.setEmail(in.nextLine());
-        user.displayMessage();
+        Scanner in = new Scanner(System.in);
+        Data[] user = new Data [100];
+        int i = 0;
+        char pil;
+
+        do {
+            user[i] = new Data();
+            System.out.print("Masukkan Nama : ");
+            user[i].setNama(in.nextLine());
+            System.out.print("Masukkan Alamat : ");
+            user[i].setAlamat(in.nextLine());
+            System.out.print("Masukkan Nomor Telepon : ");
+            user[i].setNotelepon(in.nextLine());
+            System.out.print("Masukkan Alamat E-mail : ");
+            user[i].setEmail(in.nextLine());
+            i++;
+            System.out.println("Apakah anda mau mendata lagi? (y/t)");
+            System.out.print("Pilihan Anda : ");
+            pil = in.next().toUpperCase().charAt(0);
+            in.nextLine();
+            if (pil == 'T') {
+                for (int x =0 ; x<i;x++){
+                    user[x].displayMessage();
+                }
+            }
+        } while (pil != 'T');
     }
-    
+
 }
