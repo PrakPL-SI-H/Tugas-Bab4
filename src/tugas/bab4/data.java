08f5fd5fab1ec5b9938f49935c952eec8d744c49
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-28 22:46:57 +0700

    edit method class

--- src/tugas/bab4/data.java
+++ src/tugas/bab4/data.java
@@ -9,12 +9,26 @@ private String email;
     public String getAlamat(){  //ACCESSOR
         return alamat;
     }
-    
+    public void setAlamat(String temp){ //MUTATOR
+        alamat = temp;
+    }
+    public String getNotelp (){ //ACCESSOR
+        return notlp;
+    }
+    public void setNotelp (String temp){ //MUTATOR
+        notlp = temp;
+    }
+    public String getEmail(){ //ACCESSOR
+        return email;
+    }
+    public void setEmail(String temp){  //MUTATOR
+        email = temp;
+    }
     public void tampilkan(){
         System.out.println("++++++++ Buku Telpon ++++++++");
         System.out.println("Nama : "+nama); //dari constructor
         System.out.println("Alamat : "+getAlamat());
-        System.out.println("Nomor telp : ");
-        System.out.println("Email : ");
+        System.out.println("Nomor telp : "+getNotelp());
+        System.out.println("Email : "+getEmail());
     }
 }
