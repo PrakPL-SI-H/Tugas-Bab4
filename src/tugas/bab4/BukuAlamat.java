e3f2db21ed185ee83933ab158b5e1cbab5b16714
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-03-28 21:58:29 +0700

    edit main class

--- src/tugas/bab4/BukuAlamat.java
+++ src/tugas/bab4/BukuAlamat.java
@@ -2,5 +2,13 @@ public class BukuAlamat{
     public static void main(String[]args){
         data a = new data("Prasetyo");
         data b = new data("Wibisono");
+        a.setAlamat("Pandaan");
+        a.setNotelp("089660705147");
+        a.setEmail("tiyo16@yahoo.co.id");
+        a.tampilkan();
+        b.setAlamat("Malang");
+        b.setNotelp("089660795354");
+        b.setEmail("wibi67@yahoo.co.id");
+        b.tampilkan();
     }
 }
