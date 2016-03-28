5facb19e029937ff935508ce283f47df3bacc8c5
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-03-28 22:43:55 +0700

    percobaan pertama class buku dengan method public buku

---
+++ src/tugas/bab4/Buku.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LAPORAN;
+
+
+
+import java.util.Scanner;
+/**
+ *
+ * @author TEGAR
+ */
+
+class Buku{
+    private String nama;
+    private String alamat;
+   private String nomerTelp;
+    private String email;
+    public Buku(){
+        this.nama = "";
+        this.alamat = "";
+        this.nomerTelp = "";
+        this.email =  "";
+    }
+    
+     }
+    
+ 
