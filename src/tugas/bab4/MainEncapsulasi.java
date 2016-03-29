2909bb385a9fa081504ac4ca2cc512aa3b7ca370
FelixYosa <felix_yosa@ymail.com>
2016-03-29 22:31:49 +0700

    edit main

--- src/tugas/bab4/MainEncapsulasi.java
+++ src/tugas/bab4/MainEncapsulasi.java
@@ -4,7 +4,16 @@ package mainencapsulasi;
 public class MainEncapsulasi {
 
     public static void main(String[] args) {
-        
+        Encapsulasi data1 = new Encapsulasi("Felix Yosa", "Jalan Ikan Tombro",
+        "0812345", "felixyosa@yahoo.com");
+        Encapsulasi data2 = new Encapsulasi("Herdima Sanjaya", "Jalan Ikan Hiu",
+        "0898765", "herdimasanjaya@yahoo.com");
+        Encapsulasi data3 = new Encapsulasi("Abraham Tito", "Jalan Ikan Nila",
+        "0821567", "abrahamtito@yahoo.com");
+        Encapsulasi data4 = new Encapsulasi("Agis Della", "Jalan Ikan Paus",
+        "0857891", "agisdella@yahoo.com");
+        Encapsulasi data5 = new Encapsulasi("Afesa Austin", "Jalan Ikan Gurami",
+        "0810341", "afesaaustin@yahoo.com");
     }
     
 }
