7a1cefd7d513d4d45f1ff68162e1ce81a8cffbc1
Mirza Syauqi Rafif <mirzasrafif@yahoo.com>
2016-03-29 10:16:31 +0700

    add class BukuAlamat
    berisi atribut nama, alamat, no telp, email beserta  method set get-nya

---
+++ src/BukuAlamat.java
@@ -0,0 +1,42 @@
+public class BukuAlamat {
+
+    private String nama;
+    private String alamat;
+    private String noTelp;
+    private String email;
+
+    public BukuAlamat() {
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
