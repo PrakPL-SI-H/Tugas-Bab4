9cfeb41b66ed480970390acb55ff140748486648
pandudamar <pandudpriambodo@gmail.com>
2016-03-30 04:16:34 +0700

    155150401111134_Pandu Damar Priambodo_TugasBab4

--- src/LatihanBab4.java
+++ src/LatihanBab4.java
@@ -1,13 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
+import java.util.Scanner;
+class Alamat {
+Scanner input = new Scanner(System.in);
+    private String nama;
+    private String alamat;
+    private String noTelepon;
+    private String alamatEmail;
+    public Alamat() {
+        nama = " ";
+        alamat = " ";
+        noTelepon = " ";
+        alamatEmail = " ";
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    public String getNoTelepon() {
+        return noTelepon;
+    }
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+    public String getAlamatEmail() {
+        return alamatEmail;
+    }    
+}
+
+class LatihanBab1 {
     public static void main(String[] args) {
         System.out.println("tess");
         
