12227259849658f4da37b01d0b3f31c0ad6ce30d
istighfarinba <bafarcorp@gmail.com>
2016-03-29 16:26:16 +0700

    Membuat Mutator pada class Data

--- src/Data.java
+++ src/Data.java
@@ -7,5 +7,16 @@ public class Data {
     private int noCall;
     private String emailAddress;
     
-    
+    public void setName(String a){ //Membuat Mutator setName
+        this.name=a;
+    }
+    public void setAddress(String a){ //Membuat Mutator setAddress
+        this.address=a;
+    }
+    public void setNocall(int a){ //Membuat Mutator setNocall
+        this.noCall=a;
+    }
+    public void setEmail(String a){ //Membuat Mutator setEmail
+        this.emailAddress=a;
+    }
 }
