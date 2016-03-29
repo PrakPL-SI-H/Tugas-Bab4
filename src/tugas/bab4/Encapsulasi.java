fe51dc56f94be1b28b6cdbc600697d05a8fc847e
FelixYosa <felix_yosa@ymail.com>
2016-03-29 22:11:00 +0700

    buat konstruktor

--- src/tugas/bab4/Encapsulasi.java
+++ src/tugas/bab4/Encapsulasi.java
@@ -2,5 +2,13 @@
 package mainencapsulasi;
 
 public class Encapsulasi {
+    String nama, alamat, email;
+    int telp;
     
+    public Encapsulasi(String a, String b, int c, String d){
+        nama = a;
+        alamat = b;
+        telp = c;
+        email = d;
+    }
 }
