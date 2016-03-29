79c7436488ec3307a6adbb7fb49449f4f6e9f7d8
Muh. Asdar <muh.asdar17@gmail.com>
2016-03-29 13:51:58 +0700

    perbaikan program

--- src/tugas/bab4/BukuAlamat.java
+++ src/tugas/bab4/BukuAlamat.java
@@ -2,48 +2,41 @@ package tugas.bab4;
 
 public class BukuAlamat {
 
-    private String data[]; //indeks 0 = nama, 1 = alamat, 2 = no telfon, 3 = Email
+    private String data[][]; //indeks 0 = nama, 1 = alamat, 2 = no telfon, 3 = Email
 
-    public BukuAlamat() {
-        data = new String[4];
+    public BukuAlamat(int i) {
+        data = new String[i][4];
     }
 
-    public void setNama(String nama) {
-        data[0] = nama;
+    public void setNama(String nama, int i) {
+        data[i][0] = nama;
     }
 
-    public void setAlamat(String alamat) {
-        data[1] = alamat;
+    public void setAlamat(String alamat, int i) {
+        data[i][1] = alamat;
     }
 
-    public void setNoTel(String tel) {
-        data[2] = tel;
+    public void setNoTel(String tel, int i) {
+        data[i][2] = tel;
     }
 
-    public void setEmail(String email) {
-        data[3] = email;
+    public void setEmail(String email, int i) {
+        data[i][3] = email;
     }
 
-    public String getNama() {
-        return data[0];
+    public String getNama(int i) {
+        return data[i][0];
     }
 
-    public String getAlamat() {
-        return data[1];
+    public String getAlamat(int i) {
+        return data[i][1];
     }
 
-    public String getNoTel() {
-        return data[2];
+    public String getNoTel(int i) {
+        return data[i][2];
     }
 
-    public String getEmail() {
-        return data[3];
-    }
-
-    public void showData() {
-        System.out.println("Nama          =  " + data[0]);
-        System.out.println("Alamat        =  " + data[1]);
-        System.out.println("Nomor Telepon =  " + data[2]);
-        System.out.println("Alamat E-mail =  " + data[3]);
+    public String getEmail(int i) {
+        return data[i][3];
     }
 }
--- src/tugas/bab4/TugasBab4.java
+++ src/tugas/bab4/TugasBab4.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class TugasBab4 {
 
+    public static int indeks;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        BukuAlamat data = new BukuAlamat();
+        BukuAlamat data = new BukuAlamat(1);
         int pilih;
         do {
             System.out.println("\tBUKU ALAMAT");
@@ -16,24 +18,35 @@ public class TugasBab4 {
             System.out.print("Masukan Pilihan = ");
             pilih = in.nextInt();
             System.out.println("-------------------------------------");
-            in.nextLine();
             switch (pilih) {
                 case 1:
                     System.out.println("\tIsi Data");
-                    System.out.print("Masukan Nama          = ");
-                    data.setNama(in.nextLine());
-                    System.out.print("Masukan Alamat        = ");
-                    data.setAlamat(in.nextLine());
-                    System.out.print("Masukan Nomor Telpon  = ");
-                    data.setNoTel(in.nextLine());
-                    System.out.print("Masukan Alamat E-mail = ");
-                    data.setEmail(in.nextLine());
+                    System.out.print("Masukan Banyaknya Data = ");
+                    indeks = in.nextInt();
+                    in.nextLine();
+                    data = new BukuAlamat(indeks);
                     System.out.println("-------------------------------------");
+                    for (int i = 0; i < indeks; i++) {
+                        System.out.print("Masukan Nama          = ");
+                        data.setNama(in.nextLine(), i);
+                        System.out.print("Masukan Alamat        = ");
+                        data.setAlamat(in.nextLine(), i);
+                        System.out.print("Masukan Nomor Telpon  = ");
+                        data.setNoTel(in.nextLine(), i);
+                        System.out.print("Masukan Alamat E-mail = ");
+                        data.setEmail(in.nextLine(), i);
+                        System.out.println("-------------------------------------");
+                    }
                     break;
                 case 2:
                     System.out.println("\tLihat Data");
-                    data.showData();
-                    System.out.println("-------------------------------------");
+                    for (int i = 0; i < indeks; i++) {
+                        System.out.println("Nama          =  " + data.getNama(i));
+                        System.out.println("Alamat        =  " + data.getAlamat(i));
+                        System.out.println("Nomor Telepon =  " + data.getNoTel(i));
+                        System.out.println("Alamat E-mail =  " + data.getEmail(i));
+                        System.out.println("-------------------------------------");
+                    }
                     break;
                 default:
                     if (pilih == 0) {
