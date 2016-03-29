85a72c694d9dfb698be1194fa4a42af2dca781dc
istighfarinba <bafarcorp@gmail.com>
2016-03-29 18:26:05 +0700

    mulai edit main class..isi konstruktor.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,16 +1,19 @@
-
 package addressbook;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Istighfarin Bahtiar Amry
- * @NIM    155150401111129
- * @Kelas  SI-H
+ * @NIM 155150401111129
+ * @Kelas SI-H
  */
 public class AddressBook {
 
     public static void main(String[] args) {
-
-    }
-    
-}
+        Scanner in = new Scanner(System.in);
+        Data konstruktor = new Data("Universitas Brawijaya", "Malang", 12345, "ub@gmail.com");
+        System.out.println("        ===ADDRESS BOOK UNIVERSE===");
+        System.out.println("");
+        
+}}
