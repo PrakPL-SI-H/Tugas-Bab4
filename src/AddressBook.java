4af29307a486f709f91450c44c49fd0f55b24697
istighfarinba <bafarcorp@gmail.com>
2016-03-29 16:12:34 +0700

    membuat class main bernama "AddressBook"

---
+++ src/AddressBook.java
@@ -0,0 +1,16 @@
+
+package addressbook;
+
+/**
+ *
+ * @author Istighfarin Bahtiar Amry
+ * @NIM    155150401111129
+ * @Kelas  SI-H
+ */
+public class AddressBook {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
