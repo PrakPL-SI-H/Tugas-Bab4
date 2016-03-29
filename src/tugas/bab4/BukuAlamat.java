edd54dc121a247316cdeeee00a15736dbb9b0a75
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-29 09:31:45 +0700

    membuat class BukuAlamat, menambahkan atribut, dan method set-get Nama

---
+++ src/tugas/bab4/BukuAlamat.java
@@ -0,0 +1,15 @@
+package prakproglan;
+public class BukuAlamat {
+    private String nama;
+    private String alamat;
+    private String notelp;
+    private String email;
+    
+        public void setNama (String n){
+        nama = n;
+        }
+        
+        public String getNama(){
+            return nama;
+        }
+}
