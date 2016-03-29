f2568e3bd790e17646a10db645c8aa71707e4d9a
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-03-29 09:49:00 +0700

    membuat objek dari class BukuAlamat

---
+++ src/tugas/bab4/MainBukuAlamat.java
@@ -0,0 +1,12 @@
+package prakproglan;
+public class MainBukuAlamat {
+    public static void main(String[] args) {
+        BukuAlamat satu = new BukuAlamat();
+        BukuAlamat dua = new BukuAlamat();
+        BukuAlamat tiga = new BukuAlamat();
+        BukuAlamat empat = new BukuAlamat();
+        BukuAlamat lima = new BukuAlamat();
+        
+        
+    }
+}
