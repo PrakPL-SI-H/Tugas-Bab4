662d6bfc4b08c45478278b998cda6e1d79dc3a54
istighfarinba <bafarcorp@gmail.com>
2016-03-29 16:36:11 +0700

    Penambahan Konstruktor di class Data.

--- src/Data.java
+++ src/Data.java
@@ -6,30 +6,44 @@ public class Data {
     private String address;
     private int noCall;
     private String emailAddress;
-    
-    public void setName(String a){ //Membuat Mutator setName
-        this.name=a;
+    public Data(){}
+
+    public Data(String a, String b, int c, String d) { //Membuat Konstruktor
+        setName(a);
+        setAddress(b);
+        setNocall(c);
+        setEmail(d);
     }
-    public void setAddress(String a){ //Membuat Mutator setAddress
-        this.address=a;
+
+    public void setName(String a) { //Membuat Mutator setName
+        this.name = a;
     }
-    public void setNocall(int a){ //Membuat Mutator setNocall
-        this.noCall=a;
+
+    public void setAddress(String a) { //Membuat Mutator setAddress
+        this.address = a;
     }
-    public void setEmail(String a){ //Membuat Mutator setEmail
-        this.emailAddress=a;
+
+    public void setNocall(int a) { //Membuat Mutator setNocall
+        this.noCall = a;
     }
-    
-    public String getName(){ //Membuat Accesor getName
+
+    public void setEmail(String a) { //Membuat Mutator setEmail
+        this.emailAddress = a;
+    }
+
+    public String getName() { //Membuat Accesor getName
         return name;
     }
-    public String getAddress(){ //Membuat Accesor getAddress
+
+    public String getAddress() { //Membuat Accesor getAddress
         return address;
     }
-    public int getNocall(){ //Membuat Accesor getNocall
+
+    public int getNocall() { //Membuat Accesor getNocall
         return noCall;
     }
-    public String getEmail(){ //Membuat Accesor getEmail
+
+    public String getEmail() { //Membuat Accesor getEmail
         return emailAddress;
     }
 }
