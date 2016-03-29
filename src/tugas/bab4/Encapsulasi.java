fee2defbef7cff35a598641821e6cc6a5980a801
FelixYosa <felix_yosa@ymail.com>
2016-03-29 22:35:57 +0700

    edit class

--- src/tugas/bab4/Encapsulasi.java
+++ src/tugas/bab4/Encapsulasi.java
@@ -2,13 +2,19 @@
 package mainencapsulasi;
 
 public class Encapsulasi {
-    String nama, alamat, email;
-    int telp;
+    private String nama[], alamat[], telp[], email[];
+    public static int i = 0;
     
-    public Encapsulasi(String a, String b, int c, String d){
-        nama = a;
-        alamat = b;
-        telp = c;
-        email = d;
+    public Encapsulasi(String a, String b, String c, String d){
+        setBuku(a, b, c, d);
+        i++;
     }
+    
+    public void setBuku(String a, String b, String c, String d){
+        nama[i] = a;
+        alamat[i] = b;
+        telp[i] = c;
+        email[i] = d;
+    }
+    
 }
