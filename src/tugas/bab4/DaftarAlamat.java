0cd1be98c72ce1b4055a5e92a00962776ae36e55
kevinaphandwip <kevinnaphan@Kevins-MacBook-Pro.local>
2016-03-30 13:30:54 +0700

    memperbaiki class DaftarAlamat

--- src/tugas/bab4/DaftarAlamat.java
+++ src/tugas/bab4/DaftarAlamat.java
@@ -2,17 +2,23 @@
 package tugas.bab4;
 
 public class DaftarAlamat {
-   private String[] nama , alamat , nomor ,email;
-    
+   private String[] nama = new String[100];
+   private String[] alamat = new String[100];
+   private String[] nomor = new String[100];
+   private String[] email = new String[100];   
+   private int jumlah;
+   
     DaftarAlamat(){
-        
+       jumlah = 0;
     }
     
-    void isiAlamat (String n, String a , String no , String em, int i ){
-     this.nama[i]=n;
-     this.alamat[i]=a;
-     this.nomor[i]=no;
-     this.email[i]=em;
+    void isiAlamat (String n, String a , String no , String em){
+      
+     this.nama[jumlah]=n;
+     this.alamat[jumlah]=a;
+     this.nomor[jumlah]=no;
+     this.email[jumlah]=em;
+     jumlah++;
     }
     
     public String getNama(int i){
@@ -28,4 +34,10 @@ public class DaftarAlamat {
         return email[i];
     }
     
+ public void getDaftar(){
+    for (int i = 0 ; i < jumlah ; i++){
+        System.out.println(i+".Nama :"+nama[i]);
+    }
+ }    
 }
+
