8018ac916a6d32960f64dc51e50d5d2d99465ce5
Johan Fajar <johanrubik@gmail.com>
2016-03-30 10:56:01 +0700

    3

--- src/MainBuku/src/mainbuku/MainBuku.java
+++ src/MainBuku/src/mainbuku/MainBuku.java
@@ -1,8 +1,20 @@
 package mainbuku;
 
+import java.util.Scanner;
+
 public class MainBuku {
 
     public static void main(String[] args) {
+            Scanner in  = new Scanner (System.in);
+            System.out.println(">> Tabel Data Buku Alamat <<");
+            System.out.println("1. Memasukkan Data ");
+            System.out.println("2. Edit Data");
+            System.out.println("3. Melihat Data ");
+            System.out.println("Masukkan Pilihan : ");
+            int pil = in.nextInt();
+            switch (pil){
+                case 1 : 
+            }
             
     }
 
