fb54b7231c88c104a88c0ae812acfc25a1f461b2
LiliKordina13 <lilikordinana98@gmail.com>
2016-03-30 11:29:38 +0700

    Membuat public class alamat dengan 4 parameter

---
+++ src/tugas/bab4/Alamat.java
@@ -0,0 +1,10 @@
+package alamat;
+public class Alamat {
+    private String Nama, Alamat, NomorTelp, Email;
+    public Alamat(String nama, String alamat, String notelp, String email) {
+        this.Nama = nama;
+        this.Alamat = alamat;
+        this.NomorTelp = notelp;
+        this.Email = email;
+    }
+}
