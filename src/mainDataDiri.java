779e79d7bb461f4614d4b47c9bb66864af36d481
Firdha Sabiilarrasyad <firdhasabil@gmail.com>
2016-03-30 19:49:07 +0700

    final pembuatan main class mainDataDiri (fixed)

--- src/mainDataDiri.java
+++ src/mainDataDiri.java
@@ -5,39 +5,41 @@ import java.util.Scanner;
 public class mainDataDiri {
 
     public static void main(String[] args) {
-        /**
-         * DataDiri b1 = new DataDiri(); 
-         * b1.setNamaLengkap("Ghifari Galih
-         * Ramadhan"); 
-         * b1.setAlamat("Sir Matt Busby Way, Manchester");
-         * b1.setNoTelepon("085646355141"); 
-         * b1.setEmail("ghifari.gr@gmail.com");
-           b1.DisplayMessage();
-         */
-
         Scanner in = new Scanner(System.in);
-        DataDiri user = new DataDiri();
+        DataDiri user[] = new DataDiri[5];
 
+        for (int i = 0; i < user.length; i++) {
+            user[i] = new DataDiri();
+        }
         System.out.println(" PROGRAM INPUTAN BUKU ALAMAT");
         System.out.println(" ============================");
-        System.out.println("Silahkan Masukkan Data Diri Anda");
-        System.out.println("================================");
-        System.out.print("Masukkan Nama   : ");
-        user.setNamaLengkap(in.next());
-        System.out.print("Masukkan Alamat : ");
-        user.setAlamat(in.next());
-        System.out.print("Masukkan No.Telp: ");
-        user.setNoTelepon(in.next());
-        System.out.print("Masukkan Email  : ");
-        user.setEmail(in.next());
 
-        System.out.println("======BUKU ALAMAT======");
-        System.out.println("NAMA        : " + user.getNamaLengkap());
-        System.out.println("ALAMAT      : " + user.getAlamat());
-        System.out.println("NO.TELP/HP  : " + user.getNoTelepon());
-        System.out.println("EMAIL       : " + user.getEmail());
-        System.out.println("=====================================");
+        System.out.print("Berapa user yang ingin anda masukkan : ");
+
+        int jumlahUser = in.nextInt();
+        for (int i = 0; i < jumlahUser; i++) {
+            System.out.println("================================");
+            System.out.println("Silahkan Masukkan Data Diri Anda");
+            System.out.println("================================");
+            System.out.print("Masukkan Nama   : ");
+            user[i].setNamaLengkap(in.next());
+            System.out.print("Masukkan Alamat : ");
+            user[i].setAlamat(in.next());
+            System.out.print("Masukkan No.Telp: ");
+            user[i].setNoTelepon(in.next());
+            System.out.print("Masukkan Email  : ");
+            user[i].setEmail(in.next());
+        }
+
+        for (int i = 0; i < jumlahUser; i++) {
+            System.out.println("======BUKU ALAMAT======");
+            System.out.println("NAMA        : " + user[i].getNamaLengkap());
+            System.out.println("ALAMAT      : " + user[i].getAlamat());
+            System.out.println("NO.TELP/HP  : " + user[i].getNoTelepon());
+            System.out.println("EMAIL       : " + user[i].getEmail());
+            System.out.println("=====================================");
+
+        }
         System.out.println("TERIMAKASIH ATAS PATAS PARTISIPASINYA");
     }
-
 }
