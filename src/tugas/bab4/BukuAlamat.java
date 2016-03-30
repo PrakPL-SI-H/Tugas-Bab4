56c88d4ca5ab9e3b825c48edab69ffa26bc9a418
muhammad arif fathony <onythree1@yahoo.co.id>
2016-03-30 22:50:49 +0700

    finish

--- src/tugas/bab4/BukuAlamat.java
+++ src/tugas/bab4/BukuAlamat.java
@@ -32,5 +32,22 @@ public class BukuAlamat {
     public String getEmail() {
         return email;
     }
-        
+        public static void main(String[] args) {
+        BukuAlamat ini = new BukuAlamat("Muhammad Arif Fathony", "Malang", "08135787386", "onythree1@yahoo.co.id");
+        System.out.println("Tampil Informasi : ");
+        System.out.println("Nama   : "+ini.getNama());
+        System.out.println("Alamat : "+ini.getAlamat());
+        System.out.println("Telp   : "+ini.getTelp());
+        System.out.println("Email  : "+ini.getEmail());
+        System.out.println("Memasukkan inputan : ");
+            ini.setNama("Paijo");
+            System.out.println("Nama    : "+ini.getNama());
+            ini.setAlamat("Surabaya");
+            System.out.println("Alamat  : "+ini.getAlamat());
+            ini.setTelp ("08123456789");
+            System.out.println("Telp    : "+ini.getTelp());
+            ini.setEmail("Paijo@yahoo.com");
+            System.out.println("Email   : "+ini.getEmail());
+    }
+}
 
