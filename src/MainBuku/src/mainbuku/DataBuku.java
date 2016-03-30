b82c3b64833219b80842a67ae2c3d94a2ba56a87
Johan Fajar <johanrubik@gmail.com>
2016-03-30 11:35:37 +0700

    finish

--- src/MainBuku/src/mainbuku/DataBuku.java
+++ src/MainBuku/src/mainbuku/DataBuku.java
@@ -1,35 +1,52 @@
 package mainbuku;
 
 public class DataBuku {
+
     private static String nama;
     private static String alamat;
     private static int telp;
     private static String email;
-    
-    public DataBuku (){
+
+    public DataBuku() {
     }
-    
-    public DataBuku(String nama, String alamat, int telp,String email){
-    this.nama = nama;
-    this.alamat = alamat;
-    this.telp = telp;
-    this.email = email;
+
+    public DataBuku(String nama, String alamat, int telp, String email) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.telp = telp;
+        this.email = email;
     }
-    
-    public String getNama(){
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setTelp(int telp) {
+        this.telp = telp;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNama() {
         return nama;
     }
-    
-    public String getAlamat(){
+
+    public String getAlamat() {
         return alamat;
     }
-    
-    public int getTelp(){
+
+    public int getTelp() {
         return telp;
     }
-    
-    public String getEmail(){
+
+    public String getEmail() {
         return email;
     }
-    
+
 }
--- src/MainBuku/src/mainbuku/MainBuku.java
+++ src/MainBuku/src/mainbuku/MainBuku.java
@@ -5,40 +5,80 @@ import java.util.Scanner;
 public class MainBuku {
 
     public static void main(String[] args) {
-            Scanner in  = new Scanner (System.in);
-           
-            System.out.println(">> Tabel Data Buku Alamat <<");
+        Scanner in = new Scanner(System.in);
+        for (;;) {
+            System.out.println("");
+            System.out.println("============================");
+            System.out.println(">> Menu Data Buku Alamat <<");
             System.out.println("1. Memasukkan Data ");
             System.out.println("2. Edit Data");
             System.out.println("3. Melihat Data ");
             System.out.println("4. Exit ");
-            System.out.println("Masukkan Pilihan : ");
+            System.out.print("Masukkan Pilihan : ");
             int pil = in.nextInt();
-            switch (pil){
-                case 1 : System.out.print("Masukkan Nama : ");
-                         String nama = in.next();
-                         System.out.print("Masukkan Alamat : ");
-                         String alamat = in.next();
-                         System.out.print("Masukkan No Telp. : ");
-                         int no = in.nextInt();
-                         System.out.print("Masukkan Email : ");
-                         String email = in.next();
-                         DataBuku data = new DataBuku (nama,alamat,no,email);
-                         break;
-                case 2 : break;
-                case 3 : DataBuku data2 = new DataBuku ();
-                         System.out.println("||    >>Tabel Data Buku Alamat<<  ");
-                         System.out.println("=========================================");
-                         System.out.println("||    Nama     : " + data2.getNama());
-                         System.out.println("||    Alamat   : " + data2.getAlamat());
-                         System.out.println("||    NO. Telp : " + data2.getTelp());
-                         System.out.println("||    Email    : " + data2.getEmail());
-                         System.out.println("=========================================");
-                         break;
-                    
-                    
+            switch (pil) {
+                case 1:
+                    System.out.print("Masukkan Nama : ");
+                    String nama = in.next();
+                    System.out.print("Masukkan Alamat : ");
+                    String alamat = in.next();
+                    System.out.print("Masukkan No Telp. : ");
+                    int no = in.nextInt();
+                    System.out.print("Masukkan Email : ");
+                    String email = in.next();
+                    DataBuku data = new DataBuku(nama, alamat, no, email);
+                    break;
+                case 2:
+                    DataBuku data2 = new DataBuku();
+                    System.out.println("Apa yang ingin anda edit ?");
+                    System.out.println("1. Nama ");
+                    System.out.println("2. Alamat ");
+                    System.out.println("3. No. Telp ");
+                    System.out.println("4. Email ");
+                    System.out.print("Masukkan pilihan : ");
+                    int pil2 = in.nextInt();
+                    switch (pil2) {
+                        case 1:
+                            System.out.println("Masukkan nama baru : ");
+                            data2.setNama(in.next());
+                            break;
+                        case 2:
+                            System.out.println("Masukkan alamat baru : ");
+                            data2.setAlamat(in.next());
+                            break;
+                        case 3:
+                            System.out.println("Masukkan No. Telp baru : ");
+                            data2.setTelp(in.nextInt());
+                            break;
+                        case 4:
+                            System.out.println("Masukkan Email baru : ");
+                            data2.setEmail(in.next());
+                            break;
+                        default:
+                            System.out.println("Masukkan anda salah,Piliahan tidak tersedia"
+                                    + " Pilih Edit Data lagi sekali lagi");
+                            break;
+
+                    }
+                    break;
+                case 3:
+                    DataBuku data3 = new DataBuku();
+                    System.out.println("||    >>Tabel Data Buku Alamat<<  ");
+                    System.out.println("=========================================");
+                    System.out.println("||    Nama     : " + data3.getNama());
+                    System.out.println("||    Alamat   : " + data3.getAlamat());
+                    System.out.println("||    NO. Telp : " + data3.getTelp());
+                    System.out.println("||    Email    : " + data3.getEmail());
+                    System.out.println("=========================================");
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Masukan anda tidak sesuai nomor");
+
             }
-            
+        }
     }
 
 }
