a965ea2f40b04b5dbb9bd28b9990ea4751bfb503
rodhirf <rodhirobb@gmail.com>
2016-03-30 17:53:32 +0700

    membuat method set dan get

--- src/tugas/bab4/TugasBab4.java
+++ src/tugas/bab4/TugasBab4.java
@@ -2,3 +2,34 @@ import java.util.Scanner;
 public class BukuAlamat {
 
     public String nama, alamat, nomortel, email;
+public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setNomortel(String nomortel) {
+        this.nomortel = nomortel;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getNomortel() {
+        return nomortel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
