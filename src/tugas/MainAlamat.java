adc967a59e0bc598953b81375ff35a2054000cd8
wenigustirahayu <wenigr97@gmail.com>
2016-03-30 13:13:05 +0700

    Add main class

---
+++ src/tugas/MainAlamat.java
@@ -0,0 +1,7 @@
+package mainalamat;
+public class MainAlamat {
+    public static void main(String[] args) {
+        System.out.println("\t\t DATA BUKU ALAMAT");
+        System.out.println("");
+    }
+}
