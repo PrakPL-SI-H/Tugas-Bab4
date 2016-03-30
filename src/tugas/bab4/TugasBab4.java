6fcc39774da1185f7b6a7b4e2dbcb6996ed8fe58
dyahas <arifiani.dyah@gmail.com>
2016-03-30 11:27:07 +0700

    class

--- src/tugas/bab4/TugasBab4.java
+++ src/tugas/bab4/TugasBab4.java
@@ -1,21 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugas.bab4;
+package mainbuku;
 
-/**
- *
- * @author irwin deriyan
- */
-public class TugasBab4 {
+public class bukuAlamat {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private String nama, alamat, noTelp, email;
+
+    public bukuAlamat(String nama, String alamat, String noTelp, String email) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noTelp = noTelp;
+        this.email = email;
     }
-    
+
+    public bukuAlamat() {
+
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public String getAlamat() {
+        return this.alamat;
+    }
+
+    public String getNoTelp() {
+        return this.noTelp;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+    public void tampilkan(){
+        System.out.println("====================================================");
+        System.out.printf("%25s\t%s\n","Atribut","Deskripsi");
+        System.out.println("====================================================");
+        System.out.printf("%20s\t%s\n","Nama",getNama());
+        System.out.printf("%20s\t%s\n","Alamat",getAlamat());
+        System.out.printf("%20s\t%s\n","Nomor Telepon",getNoTelp());
+        System.out.printf("%20s\t%s\n","Alamat E-Mail",getEmail());
+        System.out.println("====================================================");
+    }
 }
