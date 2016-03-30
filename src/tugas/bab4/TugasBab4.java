0c1ee121999d99bbeb647b86248c6d44f10fec86
rodhirf <rodhirobb@gmail.com>
2016-03-30 17:52:39 +0700

    membuat kelas dan deklarasi atribut

--- src/tugas/bab4/TugasBab4.java
+++ src/tugas/bab4/TugasBab4.java
@@ -1,21 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugas.bab4;
+import java.util.Scanner;
+public class BukuAlamat {
 
-/**
- *
- * @author irwin deriyan
- */
-public class TugasBab4 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+    public String nama, alamat, nomortel, email;
