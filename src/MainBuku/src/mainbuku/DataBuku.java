5cbd6ca61ad7f333f2d8b48fcff85a2b3f36444a
Johan Fajar <johanrubik@gmail.com>
2016-03-30 10:45:14 +0700

    2

--- src/MainBuku/src/mainbuku/DataBuku.java
+++ src/MainBuku/src/mainbuku/DataBuku.java
@@ -3,6 +3,18 @@ package mainbuku;
 public class DataBuku {
     private String nama;
     private String alamat;
-    private int telepon;
-    private int email;
+    private int telp;
+    private String email;
+    
+    public DataBuku (){
+    }
+    
+    public void DataBuku(String nama, String alamat, int telp,String email){
+    this.nama = nama;
+    this.alamat = alamat;
+    this.telp = telp;
+    this.email = email;
+    }
+    
+    
 }
