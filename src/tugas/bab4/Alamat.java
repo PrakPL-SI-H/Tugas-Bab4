8a909be2ad61cb17a7bb1cd70f4008dd9018c9c3
LiliKordina13 <lilikordinana98@gmail.com>
2016-03-30 14:06:09 +0700

    Tambahan Alamat

--- src/tugas/bab4/Alamat.java
+++ src/tugas/bab4/Alamat.java
@@ -7,4 +7,30 @@ public class Alamat {
         this.NomorTelp = notelp;
         this.Email = email;
     }
+    public Alamat() {
+    }
+    public void setNama(String nama) {
+        this.Nama = nama;
+    }
+    public void setAlamat(String alamat) {
+        this.Alamat = alamat;
+    }
+    public void setNoTelp(String notelp) {
+        this.NomorTelp = notelp;
+    }
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+    public String getNama() {
+        return this.Nama;
+    }
+    public String getAlamat() {
+        return this.Alamat;
+    }
+    public String getNomorTelp() {
+        return this.NomorTelp;
+    }
+    public String getEmail() {
+        return this.Email;
+    }
 }
