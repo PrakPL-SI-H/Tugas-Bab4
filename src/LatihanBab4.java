6b5ec2c1f4ac0161534f1b5a89fc04cb69e90aba
Rasyid <mrasyid1@yahoo.com>
2016-03-30 19:43:03 -0700

    Pembuatan class buku

--- src/LatihanBab4.java
+++ src/LatihanBab4.java
@@ -1,13 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author irwin deriyan
- */
-public class LatihanBab1 {
+import java.util.Scanner;
+class Buku{
+    private String nama;
+    private String alamat;
+    private String nomerTelp;
+    private String email;
+    public Buku(){
+    }
+    public void setName(String nama){
+        this.nama =" ";
+        this.nama = nama;
+    }
+    public void setAddress(String alamat){
+        this.alamat = "";
+        this.alamat = alamat;
+    }
+    public void setNumber(String nomerTelp){
+        this.nomerTelp = "";
+        this.nomerTelp = nomerTelp;
+    }
+    public void setEmail(String email){
+        this.email =  "";
+        this.email = email;
+    }
+    public String getName(){
+        return nama;
+    }
+    public String getAddress(){
+        return alamat;
+    }
+    public String getNumber(){
+        return nomerTelp;
+    }
+    public String getEmail(){
+        return email;
+    }
+}
+public class LatihanBab4 {
     public static void main(String[] args) {
         System.out.println("tess");
         
