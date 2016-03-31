0a275eb1ff9ce13a378aa63e3d2299aad3b5bea2
Rasyid <mrasyid1@yahoo.com>
2016-03-30 21:52:17 -0700

    Main class

--- src/LatihanBab4.java
+++ src/LatihanBab4.java
@@ -4,17 +4,15 @@ class Buku{
     private String alamat;
     private String nomerTelp;
     private String email;
-    public Buku(){
-    }
-    public void setName(String nama){
+    public void setNama(String nama){
         this.nama =" ";
         this.nama = nama;
     }
-    public void setAddress(String alamat){
+    public void setAlamat(String alamat){
         this.alamat = "";
         this.alamat = alamat;
     }
-    public void setNumber(String nomerTelp){
+    public void setNoTelp(String nomerTelp){
         this.nomerTelp = "";
         this.nomerTelp = nomerTelp;
     }
@@ -22,13 +20,13 @@ class Buku{
         this.email =  "";
         this.email = email;
     }
-    public String getName(){
+    public String getNama(){
         return nama;
     }
-    public String getAddress(){
+    public String getAlamat(){
         return alamat;
     }
-    public String getNumber(){
+    public String getNoTelp(){
         return nomerTelp;
     }
     public String getEmail(){
@@ -37,9 +35,31 @@ class Buku{
 }
 public class LatihanBab4 {
     public static void main(String[] args) {
-        System.out.println("tess");
-        
-        
+       Scanner in = new Scanner(System.in);
+       Buku[] Data = new Buku[5];
+       for (int i=0; i<Data.length;i++){
+           Data[i] = new Buku();
+       }
+        for (int i = 0; i < Data.length; i++) {
+            System.out.println("------=======Data Diri=======------");
+            System.out.print("Masukkan Nama   : "                     );
+            Data[i].setNama(in.next());
+            System.out.print("Masukkan Alamat : "                     );
+            Data[i].setAlamat(in.next());
+            System.out.print("Masukkan No.Telp: "                     );
+            Data[i].setNoTelp(in.next());
+            System.out.print("Masukkan Email  : "                     );
+            Data[i].setEmail(in.next());  
+        }
+        System.out.println("--------------------============================Private Address Book=====================-----------------------");
+        System.out.println("        NAMA              |         ALAMAT        |           NO CONTACT        |            EMAIL          |");    
+        for (int i = 0; i < Data.length; i++) {                      
+            System.out.print(Data[i].getNama()+"                            ");
+            System.out.print(Data[i].getAlamat()+"                          ");
+            System.out.print(Data[i].getNoTelp()+"      ");
+            System.out.println(Data[i].getEmail()+"     ");
+            System.out.println("=============================================================================================================");
+        }
     }
-    
 }
+  
